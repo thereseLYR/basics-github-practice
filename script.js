13dd6c76d29c51823e96b490cf1bd62302f19d47
thereseLYR <thereselau1902@gmail.com>
2021-08-28 14:13:18 +0800

    Initial Implementation of Secret Word Guesser. comfortable: secret word teice in a row more comfortable: secret word twice in a row 2

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+var trycount = 0;
+
+var checkGuess = function (guess) {
+  if (guess == "banana" || guess == "chisel" || guess == "faucet") {
+    return true;
+  }
+  return false;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  while (trycount <= 1) {
+    trycount = trycount + 1;
+    console.log(`current trycount: ${trycount}`);
+    if (checkGuess(input) == true) {
+      return "you won!";
+    }
+    return "Guessed wrongly. Try again.";
+  }
+  return "Too many tries. You lose.";
 };
